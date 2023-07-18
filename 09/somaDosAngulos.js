c7c4e21d3cfcbde9adb5a80b1dc9db1274a53156
Ailken <ailkencf@gmail.com>
2023-07-18 15:15:44 -0300

    exercicio 9 (Soma dos ângulos)

---
+++ 09/somaDosAngulos.js
@@ -0,0 +1,14 @@
+//S = (n - 2) * 180
+
+//S: soma dos ângulos internos do polígono
+//n: número de lados do polígono
+//a: valor de cada angulo em graus
+
+// a = S / n
+
+let n = 6;
+let S = (n - 2) * 180;
+console.log("S = " + S);
+
+let a = S / n;
+console.log("a = " + a);
