72dfa105a62a63f873e21b1cf9d3a1314861c609
Ailken <ailkencf@gmail.com>
2023-07-18 16:28:49 -0300

    exercicio 12 (Taxa de Juros)

---
+++ 12/index.js
@@ -0,0 +1,14 @@
+//i= M / C **(1/n) −1
+
+//i: taxa de juros
+//M: montante
+//C: capital inicial (valor de dinheiro inicial)
+//n: número de meses
+
+let M = 90000;
+let C = 60000;
+let n = 24;
+
+let i = (M / C) **(1/n) - 1;
+i = i * 100;
+console.log("O seu financiamento de " + C + " reais teve uma taxa de juros de " + i.toFixed(3) + ", pois após " + n + " meses você teve que pagar " + M + " reais.");
