0348dfc40cd46ea6fe2579994793cd2a4bd91a4b
Ailken <ailkencf@gmail.com>
2023-07-18 16:10:14 -0300

    exercicio 11 (Volume de uma esfera)

---
+++ 11/index.js
@@ -0,0 +1,8 @@
+// v = 3 / 4 ∗ pi ∗ r **3
+// v = volume da esfera;
+// d = diametro da esfera;
+//diamentro é sempre o dobro do raio.
+
+let r = 3
+let v = 4/3 * r**3 + " pi";
+console.log("o volume de uma esfera de raio " + r + " é " + v);
