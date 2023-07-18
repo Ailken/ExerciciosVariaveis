d64d82af45aa4b5c876d34da2de896560e4739fb
Ailken <ailkencf@gmail.com>
2023-07-18 13:39:46 -0300

    exercicio 3 (descontoTenis)

---
+++ 03/descontoTenis.js
@@ -0,0 +1,7 @@
+let meuDinheiro = 50.0;
+let valorTenis = 110.00;
+let valorDesconto = valorTenis - meuDinheiro;
+
+let descontoPercentual = valorDesconto * 100 / valorTenis;
+
+console.log(descontoPercentual.toFixed(1));
