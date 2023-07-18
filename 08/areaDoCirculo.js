8c1a8b234c385a458dd24957c1e624038126ba36
Ailken <ailkencf@gmail.com>
2023-07-18 15:04:58 -0300

    exercicio 8 (Area do Círculo)

---
+++ 08/areaDoCirculo.js
@@ -0,0 +1,15 @@
+//C=2piR
+//A = piR**2
+
+//C: comprimento do círculo
+//A: área do círculo
+//R: raio do círculo
+
+const pi = 3.14;
+let R = 1;
+
+let C = 2 * pi * 1;
+console.log("C = " + C.toFixed(1));
+
+let A = pi * R ** 2;
+console.log("A = " + A);
