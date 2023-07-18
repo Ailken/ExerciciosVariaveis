56ca4ffd7a7abbba2ba3e8be8aaa13e408d004c0
Ailken <ailkencf@gmail.com>
2023-07-18 14:43:48 -0300

    exercicio 6 (Velocidade Média)

---
+++ 06/velocidade.js
@@ -0,0 +1,9 @@
+//velocidade = distanciaPercorrida / tempo
+//m/s * 3.6 = k/h
+
+let distanciaPercorrida = 500;
+let tempo = 10;
+
+let velocidade = (distanciaPercorrida / tempo) * 3.6;
+
+console.log("Velocidade = " + velocidade + " Km/h")
